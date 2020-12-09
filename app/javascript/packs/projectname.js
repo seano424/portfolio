9d7932c1ae8ba984b99ab8d6cd095536c4a5f4ab
Sean OReilly <soreilly424@gmail.com>
2020-12-09 13:49:50 -0500

    change tab to my name on portfolio

--- app/javascript/packs/projectname.js
+++ app/javascript/packs/projectname.js
@@ -1,11 +1,23 @@
 const projectName = () => {
   const projectTab = document.getElementById("project-tab");
-  projectTab.addEventListener("click", (event) => {
-    window.addEventListener("load", (event) => {
-      console.log(projectTab.textContent);
-      projectTab.textContent = "Sean OReilly";
-    });
-  });
+
+  
+  // projectTab.addEventListener("click", (event) => {
+  //   console.log(projectTab.textContent);
+  //   projectTab.textContent = "Sean OReilly";
+  // });
+
+  // window.addEventListener("load", (event) => {
+  //   projectTab.textContent = "Sean OReilly";
+  // });
+
+  // document.addEventListener("readystatechange", (event) => {
+  //   projectTab.textContent = "Sean OReilly";
+  // });
+
+  // document.addEventListener("DOMContentLoaded", (event) => {
+  //   projectTab.textContent = "Sean OReilly";
+  // });
 };
 
 export { projectName };
