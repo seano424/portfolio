7be7fd3e36e024be5ffbbf7665caf23efcb17d3c
Sean OReilly <soreilly424@gmail.com>
2020-12-09 11:36:29 -0500

    changed links to have a different hover effect

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
-  redirect_to root_path, notice: 'Message sent successfully'
+      redirect_to root_path, notice: 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
