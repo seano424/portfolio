9e7e7eb5904a88af427bfb190653847fd9152e5c
Sean OReilly <soreilly424@gmail.com>
2020-12-09 03:03:02 -0500

    changed production gmail to those from walkthrough

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,21 +111,18 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Gmail
-  config.action_mailer_default_url_options = { host: 'https://seanoreilly.me' }
-  Rails.application.routes.default_url_options[:host] = 'https://seanoreilly.me'
+  config.action_mailer.default_url_options = { host: 'https://seanoreillyportfolio.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default charset: 'utf-8'
+
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['GMAIL_USERNAME'],
-    :password => ENV['GMAIL_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-    }
+  :user_name => ENV['GMAIL_USERNAME'],
+  :password => ENV['GMAIL_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 end
 
 
