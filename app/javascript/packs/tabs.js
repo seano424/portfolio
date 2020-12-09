6e0103eefdc4b87ab60640dccc865971b91170d1
Sean OReilly <soreilly424@gmail.com>
2020-12-09 02:32:37 -0500

    removed tab functionality

--- app/javascript/packs/tabs.js
+++ app/javascript/packs/tabs.js
@@ -1,11 +1,11 @@
-const tabs = () => {
-  const tabbies = document.querySelectorAll('.tabs')
-  tabbies.forEach((tab) => {
-    tab.addEventListener('click', e => {
-      tab.classList.toggle('active')
-    })
-  });
+// const tabs = () => {
+//   const tabbies = document.querySelectorAll('.tabs')
+//   tabbies.forEach((tab) => {
+//     tab.addEventListener('click', e => {
+//       tab.classList.toggle('active')
+//     })
+//   });
 
-};
-export { tabs };
+// };
+// export { tabs };
 
