21f6cb1983c7a560ae29761151e88c82f1a956e3
Sean OReilly <soreilly424@gmail.com>
2021-02-05 18:30:33 -0500

    trying again to duplicate deve

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,13 +116,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :domain => 'heroku.com',
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD']
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
   }
 end
 
