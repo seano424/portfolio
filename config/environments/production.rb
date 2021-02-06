c8903e6ca375acad7fef58741a85e6c28f4cb59c
Sean OReilly <soreilly424@gmail.com>
2021-02-05 21:33:03 -0500

    copied dev to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Gmail
-  config.action_mailer.default_url_options = { host: 'https://seanoreillyportfolio.herokuapp.com/' }
   # config.action_mailer.delivery_method = :smtp
 
   # ActionMailer::Base.smtp_settings = {
@@ -125,16 +124,19 @@ Rails.application.configure do
   # :enable_starttls_auto => true
   # }
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'https://seanoreillyportfolio.herokuapp.com/' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GOOGLE_USERNAME'],
-    password:             ENV['GOOGLE_PASSWORD'],
-    authentication:       'login',
-    enable_starttls_auto: true 
-  }
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GOOGLE_USERNAME'],
+    password: ENV['GOOGLE_PASSWORD']
+
 end
 
 
